9736a0f3e2b785aed2f36c31647adb05d3623506
zhangyi <dearmrzhangyi@163.com>
2026-07-04 16:06:31 +0800

    怪物卡戒指

--- gms-server/launch.bat
+++ gms-server/launch.bat
@@ -2,5 +2,5 @@
 @title BeiDou
 chcp 65001
 
-..\..\jdk-21.0.1\bin\java.exe -Dspring.config.location=.\src\main\resources\application.yml -jar .\target\BeiDou.jar
+java -Dspring.config.location=.\src\main\resources\application.yml -jar .\target\BeiDou.jar
 pause
--- gms-server/scripts-zh-CN/npc/2006.js
+++ gms-server/scripts-zh-CN/npc/2006.js
@@ -1,18 +1,21 @@
 var status;
 
+var InventoryType = Java.type('org.gms.client.inventory.InventoryType');
+
+
 // ===================== 配置区 =====================
 // 戒指阶段对应的物品ID，请根据实际配置替换
 var RING_IDS = [
-    1112000,  // 阶段1 戒指ID
-    1112000,  // 阶段2 戒指ID
-    1112000,  // 阶段3 戒指ID
-    1112000,  // 阶段4 戒指ID
-    1112000,  // 阶段5 戒指ID
-    1112000,  // 阶段6 戒指ID
-    1112000,  // 阶段7 戒指ID
-    1112000,  // 阶段8 戒指ID
-    1112000,  // 阶段9 戒指ID
-    1112000   // 阶段10 戒指ID
+    1113000,  // 阶段1 戒指ID
+    1113001,  // 阶段2 戒指ID
+    1113002,  // 阶段3 戒指ID
+    1113003,  // 阶段4 戒指ID
+    1113004,  // 阶段5 戒指ID
+    1113005,  // 阶段6 戒指ID
+    1113006,  // 阶段7 戒指ID
+    1113007,  // 阶段8 戒指ID
+    1113008,  // 阶段9 戒指ID
+    1113009   // 阶段10 戒指ID
 ];
 
 // 所需宝石ID
@@ -64,16 +67,8 @@ function action(mode, type, selection) {
         if (status == 0) {
             var monsterBook = cm.getPlayer().getMonsterBook();
 
-            // 获取各类数量
-            var totalCards = monsterBook.getTotalCards();       // 总收集卡数
-            var normalCards = monsterBook.getNormalCard();      // 普通卡数
-            var specialCards = monsterBook.getSpecialCard();    // 特殊卡数
-            var bookLevel = monsterBook.getBookLevel();         // 图鉴等级
-
             // 获取所有卡片 Map，并统计数量
             var cardsMap = monsterBook.getCards();
-            var cardCount = cardsMap.size();                    // 不同种类卡数
-
             var count = 0;
             for (const value of cardsMap.values()) {
                 if(value == 5){
@@ -81,14 +76,7 @@ function action(mode, type, selection) {
                 }
             }
 
-            var text = "#b===== 怪物卡收集兑换戒指 =====#k\r\n\r\n";
-            text += "你当前怪怪卡收集进度:\r\n";
-            text += "图鉴等级: #r" + bookLevel + "#k\r\n";
-            text += "总收集卡数: #r" + totalCards + "#k\r\n";
-            text += "普通卡数量: #r" + normalCards + "#k\r\n";
-            text += "特殊卡数量: #r" + specialCards + "#k\r\n";
-            text += "卡片种类数: #r" + cardCount + "#k\r\n";
-            text += "收集完成卡片数量: #r" + count + "#k\r\n";
+            var text = "你当前已收集完成的怪物卡种类为: #b"+count+"#k\r\n";
             text += "请选择要兑换的戒指阶段:\r\n";
 
             for (var i = 0; i < 10; i++) {
@@ -97,7 +85,7 @@ function action(mode, type, selection) {
                 var gemIcon = "#v" + GEM_IDS[i] + "#";
                 var ringIcon = "#v" + RING_IDS[i] + "#";
                 var statusStr = canClaim ? "#g[可兑换]#k" : "#r[未达标]#k";
-                text += "#L" + i + "#" + ringIcon + " 阶段" + stage + " " + statusStr + " (需" + REQUIRED_CARDS[i] + "张卡 + " + gemIcon + "×" + REQUIRED_GEMS + ")#l\r\n";
+                text += "#L" + i + "#" + ringIcon + " 阶段" + stage + " " + statusStr + " (需" + REQUIRED_CARDS[i] + "种卡 + " + gemIcon + "×" + REQUIRED_GEMS + ")#l\r\n";
             }
 
             cm.sendSimple(text);
@@ -124,7 +112,7 @@ function action(mode, type, selection) {
 
             // 检查卡组数量
             if (count < requiredCard) {
-                cm.sendOk("你的怪物卡收集数量不足。\r\n当前: #r" + count + "#k 张\r\n需要: #r" + requiredCard + "#k 张");
+                cm.sendOk("你的怪物卡收集数量不足。\r\n当前: #r" + count + "#k 种\r\n需要: #r" + requiredCard + "#k 种");
                 cm.dispose();
                 return;
             }
@@ -144,10 +132,53 @@ function action(mode, type, selection) {
                 return;
             }
 
+            // 除第一次领取外需检查戒指
+            if(selection > 0){
+                var ringOldId = RING_IDS[selection-1];
+                if(cm.haveItem(ringOldId)){
+                    //扣除旧戒指
+                    cm.gainItem(ringOldId, -1);
+                }else{
+                    cm.sendOk("你需要换取新戒指吗？把戒指放在背包里让我看到吧。");
+                    cm.dispose();
+                    return;
+                }
+            }else{
+                // 第一次需检查有没有有没有怪物戒指
+                var flag = false;
+                // 获取身上装备栏（已穿戴）
+                var equippedInv = cm.getPlayer().getInventory(InventoryType.EQUIPPED);
+                var items = equippedInv.list();
+                var iter = items.iterator();
+                while (iter.hasNext()) {
+                    var item = iter.next();
+                    var itemId = item.getItemId();      // 物品ID
+                    if (itemId>=1113000 && itemId<=1113009){
+                        flag = true;
+                    }
+                }
+                // 获取背包装备栏（未穿戴）
+                var equipInv = cm.getPlayer().getInventory(InventoryType.EQUIP);
+                var items2 = equipInv.list();
+                var iter2 = items2.iterator();
+                while (iter2.hasNext()) {
+                    var item2 = iter2.next();
+                    var itemId2 = item2.getItemId();      // 物品ID
+                    if (itemId2>=1113000 && itemId2<=1113009){
+                        flag = true;
+                    }
+                }
+                if (flag){
+                    cm.sendOk("你已经有戒指了。");
+                    cm.dispose();
+                    return;
+                }
+            }
+
             // 扣除宝石
             cm.gainItem(gemId, -REQUIRED_GEMS);
 
-            // 给予戒指
+            // 给予新戒指
             cm.gainItem(ringId, 1);
 
             cm.sendOk("恭喜！你成功兑换了 #b阶段" + stage + "#k 的戒指！\r\n\r\n" +
